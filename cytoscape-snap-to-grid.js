298a90052c0c05835c77a551665164af798f81be
mrsfy <mrsfy@outlook.com>
2016-07-12 00:33:00 +0300

    Parent nodes now draggable. #1

--- cytoscape-snap-to-grid.js
+++ cytoscape-snap-to-grid.js
@@ -84,12 +84,26 @@ module.exports = function (cy, snap) {
 
     var attachedNode;
 
+    function moveTopDown(children, dx, dy) {
+        for(var i = 0; i < children.length; i++){
+            var child = children[i];
+            child.position({
+                x: child.position('x') + dx,
+                y: child.position('y') + dy
+            });
+            snap.snapNode(child);
+
+            moveTopDown(child.children(), dx, dy);
+        }
+    }
+
     function tapDrag(e) {
         var nodePos = attachedNode.position();
         var mousePos = snap.snapPos(e.cyPosition);
         if (nodePos.x != mousePos.x || nodePos.y != mousePos.y){
             attachedNode.unlock();
-            snap.snapNode(attachedNode, mousePos);
+            moveTopDown(attachedNode, mousePos.x - nodePos.x, mousePos.y - nodePos.y);
+            //snap.snapNode(attachedNode, mousePos);
             attachedNode.lock();
             attachedNode.trigger("drag");
         }
