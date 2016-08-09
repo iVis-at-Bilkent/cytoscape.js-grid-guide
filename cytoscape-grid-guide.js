3f06c6b4a7265f3e3e3e0b66599ec188bb98b851
mrsfy <mrsfy@outlook.com>
2016-08-09 19:09:18 +0300

    render sizes/dimensions inside

--- cytoscape-grid-guide.js
+++ cytoscape-grid-guide.js
@@ -1856,14 +1856,14 @@ module.exports = function (opts, cy, $, debounce) {
 
     lines.getDims = function (node) {
 
-        var pos = node.renderedPosition();
-        var width = node.renderedWidth();
-        var height = node.renderedHeight();
+        var pos = lines.renderPos(node.position());
+        var width = lines.renderDim(node.width());
+        var height = lines.renderDim(node.height());
         var padding = {
-            left: Number(node.renderedStyle("padding-left").replace("px", "")),
-            right: Number(node.renderedStyle("padding-right").replace("px", "")),
-            top: Number(node.renderedStyle("padding-top").replace("px", "")),
-            bottom: Number(node.renderedStyle("padding-bottom").replace("px", ""))
+            left: Number(lines.renderDim(node.style("padding-left").replace("px", ""))),
+            right: Number(lines.renderDim(node.style("padding-right").replace("px", ""))),
+            top: Number(lines.renderDim(node.style("padding-top").replace("px", ""))),
+            bottom: Number(lines.renderDim(node.style("padding-bottom").replace("px", "")))
         };
 
         // v for vertical, h for horizontal
@@ -1903,17 +1903,25 @@ module.exports = function (opts, cy, $, debounce) {
 
     lines.destroy = function () {
         Tree = null;
+        lines.clear();
     };
 
     lines.clear = clearDrawing;
 
+    lines.renderDim = function (val) {
+        return val * cy.zoom();
+    };
     lines.renderPos = function (pos) {
-        return pos*cy.zoom() + cy.pan();
+        var pan = cy.pan();
+        return {
+            x: lines.renderDim(pos.x) + pan.x,
+            y: lines.renderDim(pos.y) + pan.y
+        };
     };
 
     lines.drawLine = function (fromNode, toNode) {
-        var from = fromNode.renderedPosition();
-        var to = toNode.renderedPosition();
+        var from = lines.renderPos(fromNode.position());
+        var to = lines.renderPos(toNode.position());
         ctx.beginPath();
         ctx.moveTo(from.x,from.y);
         ctx.lineTo(to.x,to.y);
